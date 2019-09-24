50b98f47891b8a4e1aba2a7cdda398fa00a55242
Benjamin Szmodis <bls223@lehigh.edu>
2019-09-24 16:00:39 -0400

    Edited Intro to R.R

--- Intro to R.R
+++ Intro to R.R
@@ -1,5 +1,5 @@
 # R script to accompany Intro to R
-# Copy for bls223
+# Copy for bls223 / AKA Ben Szmodis
 ###################################################
 
 # You will see that you can put non-executable "comment" lines
