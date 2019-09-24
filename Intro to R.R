8f559f863a65baa42f3ceaeebf442a403399877e
Benjamin Szmodis <bls223@lehigh.edu>
2019-09-24 15:46:32 -0400

    Edited Header

--- Intro to R.R
+++ Intro to R.R
@@ -1,6 +1,6 @@
-# R script to accopany Intro to R
-# Copy for RStudent 2019
-######################################################
+# R script to accompany Intro to R
+# Copy for bls223
+###################################################
 
 # You will see that you can put non-executable "comment" lines
 # in your script by putting "#" at the beginning of the lines.
